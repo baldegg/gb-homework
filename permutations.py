bd637790795b58b9d1ac342919a10fb14b08b7bc
Richard <richard.mabley@gmail.com>
2018-06-16 15:15:23 -0500

    fixed permutation algoritm

--- permutations.py
+++ permutations.py
@@ -1,6 +1,6 @@
 import sys  # To use sys.argv to read command line arguments
 
-permutations = []  # Initialize array of permutations
+permutations = []  # Initialize list of permutations
 
 """Loads file from command line argument.  Prints error and exits if file not found.  Otherwise,
 reads file line by line and returns list of strings to be to be permuted."""
@@ -14,41 +14,31 @@ def loadFile(file):
         exit(-1)
 
 
-"""Accepts a single string and returns a comma separated string of all its unique permutations.  
+"""Accepts a single string and appends to permutations list all unique permutations.  
 By default, starts iterating at the start of string; start value is only changed when the function is called
 recursively."""
 def permute(string, start=0):
-    # Converts string to list of characters so we can use their indices and swap them around more easily.
-    chars = list(string)
-
     # If we are starting from the beginning (ie not being called by recursion), re-initialize our array of permutations
     # so that we don't include permutation sets of previous strings.
     if start == 0:
         permutations.clear()
-        print(chars)
 
     # If we are starting at the end of the string, this means we have reached the bottom of a recursion branch.
     # Therefore, we have completed one of our permutations and add it to our list.
-    if start == len(chars):
-        permutations.append(''.join(chars))
+    if start == len(string):
+        permutations.append(''.join(string))
 
     # Otherwise, iterate through the string, swapping characters at starting point and iteration counter.
     else:
-        for i in range(start, len(chars)):
-            chars[start], chars[i] = chars[i], chars[start]
+        for i in range(start, len(string)):
+            string[start], string[i] = string[i], string[start]
             # Recursively call the permutation function on the mutated string starting at one place to the right
             # of the current loop. In this manner, every character is swapped with every other character as the start
             # counter approaches the end of the string.
-            permute(chars, start + 1)
+            permute(string, start + 1)
             # We then swap the character back so we can continue iterating through the unmutated string once
             # the recursion calls complete.
-            chars[start], chars[i] = chars[i], chars[start]
-
-    # We want the permutations to be a comma separated string, not a list, so we join them with commas.
-    # They should be sorted 1-9, A-Z, a-z.  Luckily, by default python's sorted function uses ASCII byte values, which
-    # follow this same format, so we can use it as is.
-    comma_perms = ','.join(sorted(permutations))
-    return comma_perms
+            string[start], string[i] = string[i], string[start]
 
 
 if __name__ == "__main__":
@@ -56,9 +46,16 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         infile = sys.argv[1]
         strings = loadFile(infile)
-        # Prints output of permute function on each line of file.
+        # Runs permute on output of permute function on each line of file.
         for string in strings:
-            print(permute(string))
+            # We use python's sorted function on each string before sending it to the permute function because it is
+            # less expensive to sort a single string than a possibly huge list of permutations.
+            # We want the final list to be sorted ascending 0-9, A-Z, a-z.  Luckily this corresponds to ASCII byte
+            # values, which the sorted function uses to sort, so we can just use it directly.
+            permute(sorted(string))
+            # For every iteration, we print the contents of the permutation list joined by commas because it gets
+            # wiped when the next string is fed to the permute function.
+            print(','.join(permutations))
     else:
         print("Usage: Specify a filename containing line-separated strings to permute.")
 
